ad29bc2dda159f00652832954ef1794a4e2532aa
Hailey Magoline <magoline.hailey@gmail.com>
2022-02-07 19:22:16 -0600

    get all users complete

--- api/server.js
+++ api/server.js
@@ -1,6 +1,25 @@
 // BUILD YOUR SERVER HERE
 const express = require("express");
+const User = require("./users/model");
 
 const server = express();
+
+server.use(express.json());
+
+server.get("/hello", (req, res) => {
+  res.json({ message: "hello" });
+});
+
+server.get("/api/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({
+      message: "Error fetching all users",
+      error: err.message,
+    });
+  }
+});
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
